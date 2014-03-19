09d8ed16d9e857b3d38874f4f69f2323e73e8dbc
Dylan W. Schwilk <dylan@schwilk.org>
2014-03-19 10:29:02 -0500

    Modified tree selection to use newer csv files, fixed inconsistencies in csv files.

--- tree-selection/GUMO/gumo-tree-list.r
+++ tree-selection/GUMO/gumo-tree-list.r
@@ -14,7 +14,11 @@ strip.extension <- function(s){
   }
 
 
-tfiles <- list.files(path="./taylor-data", pattern="*.csv",full.names=TRUE)
+#tfiles <- list.files(path="./taylor-data", pattern="*.csv",full.names=TRUE)
+
+# new data filtered on 15m from trail sent by Helen 2014-03-19
+tfiles <- list.files(path="./plot_latlon", pattern="*.csv",full.names=TRUE)
+
 species <- strip.extension(basename(tfiles))
 
 treelist <- data.frame()
@@ -22,19 +26,25 @@ treelist <- data.frame()
 for (tf in tfiles) {
     d <- read.csv(tf, stringsAsFactors=FALSE)
     n <- strip.extension(basename(tf))[1]
-    names(d) <- c("plot", "UTM.E", "UTM.N", "IV")
+    # fix names as file header hsa some trailing underscores, etc
+    names(d) <- c("plot", "UTM.E", "UTM.N", "IV", "NEAR.FID", "NEAR.DIST", "lon", "lat", "map.label")
     d$spcode <- n
+    print( n)
     treelist <- rbind(treelist, d)
 }
     
 # convert from UTM zone 13 to lat lon
 points <- SpatialPoints(treelist[,2:3], proj4string=CRS("+proj=utm +zone=13"))
 latlon <- spTransform(points, CRS("+proj=longlat"))
-treelist[,2:3] <- coordinates(latlon)
+treelist$newlon <- coordinates(latlon)[,1]
+treelist$newlat <- coordinates(latlon)[,2]
 
-names(treelist) <- c("plot", "lon", "lat", "IV", "spcode")
+# map label is plot if it does not yet exist (everything except JUDE2)
+treelist$map.label[is.na(treelist$map.label)] <- treelist$plot[is.na(treelist$map.label)]
+
 write.csv(treelist, "gumo-tree-plots.csv", row.names=FALSE)
 
+# redo lat/lon because data in csv files does not look to ahve correct precision?
 gpsexport <- data.frame(Name=paste(treelist$spcode,treelist$plot, sep=""), Latitude=treelist$lat, Longitude=treelist$lon)
 write.csv(gpsexport, "gumo-tree-plots-gps-export.csv", row.names=FALSE)
 
