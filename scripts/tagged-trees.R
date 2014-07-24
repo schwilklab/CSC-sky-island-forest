48dfb13813fb7f0a233e857b66139ce3327efd04
Dylan W. Schwilk <dylan@schwilk.org>
2014-07-24 14:13:37 -0500

    Fix sig digits in tagged_trees wp
    GPS stores more digits than displays, so correct waypoints from raw GPS
    donwloads. Unfortunately, gps wp from last trip (DM late July) are lost because
    were overwritten. We have the hand-entered values, however.

--- scripts/tagged-trees.R
+++ scripts/tagged-trees.R
@@ -7,3 +7,15 @@ trees <- read.csv("../data/tagged_trees.csv")
 
 # check number of tagged trees by mtn range and by species
 ddply(trees, .(mtn, spcode), summarize, count = length(tag))
+
+
+
+# get more sig digits from raw gps downlaods. Still need to do this for late
+# July dm trip
+## gps <- read.csv("../data/csc-gps-trees.csv")
+## m <- merge(trees, gps, all.x=TRUE)
+## m$lat[!is.na(m$Latitude)] <- m$Latitude[!is.na(m$Latitude)]
+## m$lon[!is.na(m$Longitude)] <- m$Longitude[!is.na(m$Longitude)]
+
+## write.csv(m, "new-tagged-trees.csv")
+
